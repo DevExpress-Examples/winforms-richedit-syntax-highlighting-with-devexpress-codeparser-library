f779690934f6141dd53e6ce4c08fc2567ac1d956
DevExpressExampleBot <fakeEmail>
2023-12-08 16:26:36 +0400

    Source auto update [skip ci]

--- VB/WindowsFormsApplication1/Form1.vb
+++ VB/WindowsFormsApplication1/Form1.vb
@@ -1,30 +1,28 @@
-﻿Imports Microsoft.VisualBasic
 Imports System
-Imports System.Collections.Generic
 Imports System.ComponentModel
-Imports System.Data
 Imports System.Drawing
-Imports System.Linq
-Imports System.Text
 Imports System.Windows.Forms
 Imports DevExpress.XtraRichEdit
 Imports DevExpress.XtraRichEdit.Services
 
 Namespace WindowsFormsApplication1
-	Partial Public Class Form1
-		Inherits Form
-		Public Sub New()
-			InitializeComponent()
-		End Sub
 
-		Private Sub Form1_Load(ByVal sender As Object, ByVal e As EventArgs) Handles MyBase.Load
-			richEditControl1.ReplaceService(Of ISyntaxHighlightService)(New HTMLSyntaxHighlightService(richEditControl1))
-			Using server As IRichEditDocumentServer = richEditControl1.CreateDocumentServer()
-				server.Text = "some HTML text"
-				richEditControl1.Text = server.HtmlText
-			End Using
-			richEditControl1.Document.DefaultCharacterProperties.FontName = "Consolas"
-			richEditControl1.Document.DefaultCharacterProperties.FontSize = 9
-		End Sub
-	End Class
+    Public Partial Class Form1
+        Inherits Form
+
+        Public Sub New()
+            InitializeComponent()
+        End Sub
+
+        Private Sub Form1_Load(ByVal sender As Object, ByVal e As EventArgs)
+            richEditControl1.ReplaceService(Of ISyntaxHighlightService)(New HTMLSyntaxHighlightService(richEditControl1))
+            Using server As IRichEditDocumentServer = richEditControl1.CreateDocumentServer()
+                server.Text = "some HTML text"
+                richEditControl1.Text = server.HtmlText
+            End Using
+
+            richEditControl1.Document.DefaultCharacterProperties.FontName = "Consolas"
+            richEditControl1.Document.DefaultCharacterProperties.FontSize = 9
+        End Sub
+    End Class
 End Namespace
--- VB/WindowsFormsApplication1/SyntaxHighlighting.vb
+++ VB/WindowsFormsApplication1/SyntaxHighlighting.vb
@@ -1,8 +1,4 @@
-﻿Imports Microsoft.VisualBasic
-Imports System
 Imports System.Collections.Generic
-Imports System.Linq
-Imports System.Text
 Imports DevExpress.XtraRichEdit.Services
 Imports DevExpress.XtraRichEdit.API.Native
 Imports DevExpress.CodeParser
@@ -10,88 +6,81 @@ Imports System.Drawing
 Imports DevExpress.XtraRichEdit
 
 Namespace WindowsFormsApplication1
-	Public Class HTMLSyntaxHighlightService
-		Implements ISyntaxHighlightService
-		Private ReadOnly syntaxEditor As RichEditControl
 
-		Private TokensMapping As New Dictionary(Of TokenCategory, SyntaxHighlightProperties)()
+    Public Class HTMLSyntaxHighlightService
+        Implements ISyntaxHighlightService
 
-		Private textProperties As SyntaxHighlightProperties
+        Private ReadOnly syntaxEditor As RichEditControl
 
-		Private Sub AddTokensMapping(ByVal category As TokenCategory, ByVal foreColor As Color)
-			Dim tokenProperties As New SyntaxHighlightProperties()
-			tokenProperties.ForeColor = foreColor
-			TokensMapping.Add(category, tokenProperties)
-		End Sub
+        Private TokensMapping As Dictionary(Of TokenCategory, SyntaxHighlightProperties) = New Dictionary(Of TokenCategory, SyntaxHighlightProperties)()
 
-		Private Function GetTokensMapping(ByVal category As TokenCategory) As SyntaxHighlightProperties
-			If TokensMapping.ContainsKey(category) Then
-				Return TokensMapping(category)
-			Else
-				Return textProperties
-			End If
-		End Function
+        Private textProperties As SyntaxHighlightProperties
 
-		Public Sub New(ByVal syntaxEditor As RichEditControl)
-			Me.syntaxEditor = syntaxEditor
-		End Sub
+        Private Sub AddTokensMapping(ByVal category As TokenCategory, ByVal foreColor As Color)
+            Dim tokenProperties As SyntaxHighlightProperties = New SyntaxHighlightProperties()
+            tokenProperties.ForeColor = foreColor
+            TokensMapping.Add(category, tokenProperties)
+        End Sub
 
-		Private Sub HighlightSyntax(ByVal tokens As TokenCollection)
-			If TokensMapping.Count = 0 Then
-				AddTokensMapping(TokenCategory.HtmlAttributeName, Color.Red)
-				AddTokensMapping(TokenCategory.HtmlAttributeValue, Color.Blue)
-				AddTokensMapping(TokenCategory.HtmlComment, Color.Green)
-				AddTokensMapping(TokenCategory.HtmlElementName, Color.Brown)
-				AddTokensMapping(TokenCategory.HtmlEntity, Color.Green)
-				AddTokensMapping(TokenCategory.HtmlOperator, Color.Blue)
-				AddTokensMapping(TokenCategory.HtmlServerSideScript, Color.Gray)
-				AddTokensMapping(TokenCategory.HtmlString, Color.Black)
-				AddTokensMapping(TokenCategory.HtmlTagDelimiter, Color.Blue)
-				AddTokensMapping(TokenCategory.CssComment, Color.Green)
-				AddTokensMapping(TokenCategory.CssKeyword, Color.Red)
-				AddTokensMapping(TokenCategory.CssPropertyName, Color.Red)
-				AddTokensMapping(TokenCategory.CssPropertyValue, Color.Blue)
-				AddTokensMapping(TokenCategory.CssSelector, Color.Brown)
-				AddTokensMapping(TokenCategory.CssStringValue, Color.Blue)
+        Private Function GetTokensMapping(ByVal category As TokenCategory) As SyntaxHighlightProperties
+            If TokensMapping.ContainsKey(category) Then
+                Return TokensMapping(category)
+            Else
+                Return textProperties
+            End If
+        End Function
 
-				textProperties = New SyntaxHighlightProperties()
-				textProperties.ForeColor = Color.Black
-			End If
+        Public Sub New(ByVal syntaxEditor As RichEditControl)
+            Me.syntaxEditor = syntaxEditor
+        End Sub
 
-			If tokens Is Nothing OrElse tokens.Count = 0 Then
-				Return
-			End If
+        Private Sub HighlightSyntax(ByVal tokens As TokenCollection)
+            If TokensMapping.Count = 0 Then
+                AddTokensMapping(TokenCategory.HtmlAttributeName, Color.Red)
+                AddTokensMapping(TokenCategory.HtmlAttributeValue, Color.Blue)
+                AddTokensMapping(TokenCategory.HtmlComment, Color.Green)
+                AddTokensMapping(TokenCategory.HtmlElementName, Color.Brown)
+                AddTokensMapping(TokenCategory.HtmlEntity, Color.Green)
+                AddTokensMapping(TokenCategory.HtmlOperator, Color.Blue)
+                AddTokensMapping(TokenCategory.HtmlServerSideScript, Color.Gray)
+                AddTokensMapping(TokenCategory.HtmlString, Color.Black)
+                AddTokensMapping(TokenCategory.HtmlTagDelimiter, Color.Blue)
+                AddTokensMapping(TokenCategory.CssComment, Color.Green)
+                AddTokensMapping(TokenCategory.CssKeyword, Color.Red)
+                AddTokensMapping(TokenCategory.CssPropertyName, Color.Red)
+                AddTokensMapping(TokenCategory.CssPropertyValue, Color.Blue)
+                AddTokensMapping(TokenCategory.CssSelector, Color.Brown)
+                AddTokensMapping(TokenCategory.CssStringValue, Color.Blue)
+                textProperties = New SyntaxHighlightProperties()
+                textProperties.ForeColor = Color.Black
+            End If
 
-			Dim document As Document = syntaxEditor.Document
-			Dim syntaxTokens As New List(Of SyntaxHighlightToken)(tokens.Count)
-			For Each token As Token In tokens
-				HighlightCategorizedToken(CType(token, CategorizedToken), syntaxTokens)
-			Next token
-			document.ApplySyntaxHighlight(syntaxTokens)
-		End Sub
+            If tokens Is Nothing OrElse tokens.Count = 0 Then Return
+            Dim document As Document = syntaxEditor.Document
+            Dim syntaxTokens As List(Of SyntaxHighlightToken) = New List(Of SyntaxHighlightToken)(tokens.Count)
+            For Each token As Token In tokens
+                HighlightCategorizedToken(CType(token, CategorizedToken), syntaxTokens)
+            Next
 
-		Private Sub HighlightCategorizedToken(ByVal token As CategorizedToken, ByVal syntaxTokens As List(Of SyntaxHighlightToken))
-			Dim backColor As Color = syntaxEditor.ActiveView.BackColor
-			Dim category As TokenCategory = token.Category
-			syntaxTokens.Add(SetTokenColor(token, GetTokensMapping(category), backColor))
+            document.ApplySyntaxHighlight(syntaxTokens)
+        End Sub
 
-		End Sub
-		Private Function SetTokenColor(ByVal token As Token, ByVal foreColor As SyntaxHighlightProperties, ByVal backColor As Color) As SyntaxHighlightToken
-			If syntaxEditor.Document.Paragraphs.Count < token.Range.Start.Line Then
-				Return Nothing
-			End If
-			Dim paragraphStart As Integer = DocumentHelper.GetParagraphStart(syntaxEditor.Document.Paragraphs(token.Range.Start.Line - 1))
-			Dim tokenStart As Integer = paragraphStart + token.Range.Start.Offset - 1
-			If token.Range.End.Line <> token.Range.Start.Line Then
-				paragraphStart = DocumentHelper.GetParagraphStart(syntaxEditor.Document.Paragraphs(token.Range.End.Line - 1))
-			End If
+        Private Sub HighlightCategorizedToken(ByVal token As CategorizedToken, ByVal syntaxTokens As List(Of SyntaxHighlightToken))
+            Dim backColor As Color = syntaxEditor.ActiveView.BackColor
+            Dim category As TokenCategory = token.Category
+            syntaxTokens.Add(SetTokenColor(token, GetTokensMapping(category), backColor))
+        End Sub
 
-			Dim tokenEnd As Integer = paragraphStart + token.Range.End.Offset - 1
+        Private Function SetTokenColor(ByVal token As Token, ByVal foreColor As SyntaxHighlightProperties, ByVal backColor As Color) As SyntaxHighlightToken
+            If syntaxEditor.Document.Paragraphs.Count < token.Range.Start.Line Then Return Nothing
+            Dim paragraphStart As Integer = DocumentHelper.GetParagraphStart(syntaxEditor.Document.Paragraphs(token.Range.Start.Line - 1))
+            Dim tokenStart As Integer = paragraphStart + token.Range.Start.Offset - 1
+            If token.Range.End.Line <> token.Range.Start.Line Then paragraphStart = DocumentHelper.GetParagraphStart(syntaxEditor.Document.Paragraphs(token.Range.End.Line - 1))
+            Dim tokenEnd As Integer = paragraphStart + token.Range.End.Offset - 1
+            Return New SyntaxHighlightToken(tokenStart, tokenEnd - tokenStart, foreColor)
+        End Function
 
-			Return New SyntaxHighlightToken(tokenStart, tokenEnd - tokenStart, foreColor)
-		End Function
-
-		#Region "#ISyntaxHighlightServiceMembers"
+'#Region "#ISyntaxHighlightServiceMembers"
         Public Sub Execute() Implements ISyntaxHighlightService.Execute
             Dim newText As String = syntaxEditor.Text
             ' Use DevExpress.CodeParser to parse text into tokens.
@@ -104,6 +93,6 @@ Namespace WindowsFormsApplication1
         Public Sub ForceExecute() Implements ISyntaxHighlightService.ForceExecute
             Execute()
         End Sub
-		#End Region ' #ISyntaxHighlightServiceMembers
-	End Class
+'#End Region  ' #ISyntaxHighlightServiceMembers
+    End Class
 End Namespace
